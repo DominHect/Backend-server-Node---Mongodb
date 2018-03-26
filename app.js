e7d2bc844527c06f68ffb62ed2b6f9099cf740d5
Héctor Domínguez <info@hectordominguez.es>
2018-03-26 19:11:47 +0200

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-//Inicializar variables
+// Inicializar variables
 var app = express();
 
-//Conexión a la base de datos
 
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res ) => {
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+ 
 
-    if( err ) throw err;
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
-    console.log('Base de datos: \x1b[34m%s\x1b[0m','online');
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
 
 // Rutas
-app.get('/', (req, res, next) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({ ok: true, mensaje: 'Petición realizada correctamente' })
-
-})
 
 // Escuchar peticiones
 app.listen(3000, () => {
-    console.log('Express server puerto 3000: \x1b[34m%s\x1b[0m','online');
-})
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
